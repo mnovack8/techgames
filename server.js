85fddaf6f55eb7b933ed0303f58d120f6ab1d20c
mnovack8 <64230125+mnovack8@users.noreply.github.com>
2026-04-25 14:19:54 -0400

    Updated use case

--- server.js
+++ server.js
@@ -3373,8 +3373,6 @@ const server = http.createServer((req, res) => {
   else if (pathname === '/about' || pathname === '/about.html') pathname = '/about.html';
   else if (pathname === '/corporate-training' || pathname === '/corporate-training.html') pathname = '/corporate-training.html';
   else if (pathname === '/specialized-training' || pathname === '/specialized-training.html') pathname = '/specialized-training.html';
-  else if (pathname === '/classrooms' || pathname === '/classrooms.html') pathname = '/classrooms.html';
-  else if (pathname === '/curious-minds' || pathname === '/curious-minds.html') pathname = '/curious-minds.html';
   else if (pathname === '/admin' || pathname === '/admin.html') pathname = '/admin.html';
   else if (pathname === '/blog'  || pathname === '/blog.html')  pathname = '/blog.html';
   else if (pathname.startsWith('/blog/') && !path.extname(pathname)) pathname = pathname + '.html';
